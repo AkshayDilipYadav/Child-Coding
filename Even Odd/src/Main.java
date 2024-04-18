3db1c1bce54d19151ecb1f91e9f1ba68956945d5
Ace <152076508+AkshayDilipYadav@users.noreply.github.com>
2024-04-18 16:53:41 +0530

    Add files via upload

---
+++ Even Odd/src/Main.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main {
+    public static void main (String[] args){
+Scanner sc = new Scanner(System.in);
+int n = sc.nextInt();
+if (n%2==0){System.out.print(0);}
+else {System.out.print(1);}
+    }
+}
